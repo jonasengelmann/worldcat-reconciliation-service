b94cb03063d0ab683cf9281389fc44e88db27407
jonasengelmann <40031476+jonasengelmann@users.noreply.github.com>
2022-11-16 17:10:47 +0100

    added webdriver options

--- worldcat_api.py
+++ worldcat_api.py
@@ -141,6 +141,13 @@ class WorldcatAPI:
 
     def get_worldcat_cookie(self) -> http.cookiejar.Cookie:
         options = Options()
+        options.add_argument("start-maximized")
+        options.add_argument("disable-infobars")
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--headless")
         driver = webdriver.Remote(
             command_executor=self.remote_webdriver_address, options=options
         )
